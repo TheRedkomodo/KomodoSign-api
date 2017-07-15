8228ec406bc3fce3162d767c8e1988c76059cfb7
Ty <tlgreen16@gmail.com>
2017-07-14 23:00:15 -0400

    added routes and added organization

---
+++ aapi/routes/document.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+
+router.get('/all/:userid', function(req, res, next) {
+  
+  
+  
+  
+  });
+
+router.post('/:docid/:userid', function(req, res, next) {
+  
+  
+  
+  
+  });
+
+router.post('/upload/:userid', function(req, res, next) {
+  
+  
+  
+  
+  });
+
+router.post('/delete/:docid/', function(req, res, next) {
+  
+  
+  
+  
+  });
+
+router.post('/send/:userid/', function(req, res, next) {
+  
+  
+  
+  
+  });
+
+router.get('/status/:docid/', function(req, res, next) {
+  
+  
+  
+  
+  });
+
+
+module.exports = router;
